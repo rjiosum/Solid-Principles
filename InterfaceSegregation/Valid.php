9758796af68adb2749b0be97ef1623a67e3f1763
Raj <rj.iosum@gmail.com>
2020-02-20 00:10:04 +0000

    Req0158 added interface segregation principle example

---
+++ InterfaceSegregation/Valid.php
@@ -0,0 +1,96 @@
+<?php declare(strict_types=1);
+
+namespace App\InterfaceSegregation\Valid;
+
+interface PaymentInterface
+{
+    public function pay();
+}
+interface FraudCheckInterface
+{
+    public function fraudCheck();
+}
+interface ThreeDSecureCheckInterface
+{
+    public function threeDSecureCheck();
+}
+interface PaymentProcessInterface
+{
+    public function process();
+}
+
+
+class PayPal implements PaymentProcessInterface, PaymentInterface
+{
+    public function pay()
+    {
+        return 'Paid by PayPal';
+    }
+
+    public function process()
+    {
+        $this->pay();
+    }
+}
+
+class SagePay implements PaymentProcessInterface, PaymentInterface, FraudCheckInterface
+{
+    public function pay()
+    {
+        return 'Paid by SagePay';
+    }
+
+    public function fraudCheck()
+    {
+        return 'Card is valid';
+    }
+
+    public function process()
+    {
+        $this->fraudCheck();
+        $this->pay();
+    }
+}
+
+class WorldPay implements PaymentProcessInterface, PaymentInterface, FraudCheckInterface, ThreeDSecureCheckInterface
+{
+    public function pay()
+    {
+        return 'Paid by WorldPay';
+    }
+
+    public function fraudCheck()
+    {
+        return 'Card is valid';
+    }
+
+    public function threeDSecureCheck()
+    {
+        return '3D secured is ok.';
+    }
+
+    public function process()
+    {
+        $this->fraudCheck();
+        $this->threeDSecureCheck();
+        $this->pay();
+    }
+}
+
+class Checkout
+{
+    private $payment;
+
+    public function __construct(PaymentProcessInterface $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    public function takePayment()
+    {
+        $this->payment->process();
+    }
+}
+
+$checkout = new Checkout(new PayPal());
+$checkout->takePayment();
---
+++ InterfaceSegregation/Violate.php
@@ -0,0 +1,75 @@
+<?php declare(strict_types=1);
+
+namespace App\InterfaceSegregation\Violate;
+
+interface PaymentInterface
+{
+    public function pay();
+    public function fraudCheck();
+    public function threeDSecureCheck();
+}
+
+class PayPal implements PaymentInterface
+{
+    public function pay()
+    {
+        return 'Paid by PayPal';
+    }
+    //client should be forced to depend on method it does not use
+    public function fraudCheck() {}
+    public function threeDSecureCheck() {}
+}
+
+class SagePay implements PaymentInterface
+{
+    public function pay()
+    {
+        return 'Paid by SagePay';
+    }
+
+    public function fraudCheck()
+    {
+        return 'Card is valid';
+    }
+    //client should be forced to depend on method it does not use
+    public function threeDSecureCheck() {}
+}
+
+class WorldPay implements PaymentInterface
+{
+    public function pay()
+    {
+        return 'Paid by WorldPay';
+    }
+
+    public function fraudCheck()
+    {
+        return 'Card is valid';
+    }
+
+    public function threeDSecureCheck()
+    {
+        return '3D secured is ok.';
+    }
+}
+
+class Checkout
+{
+    private $payment;
+
+    public function __construct(PaymentInterface $payment)
+    {
+        $this->payment = $payment;
+    }
+
+    public function takePayment()
+    {
+        $this->payment->fraudCheck();
+        $this->payment->threeDSecureCheck();
+        $this->payment->pay();
+    }
+}
+
+//PayPal class do not do perform fraudCheck or threeDSecure, but we are forcing those methods
+$checkout = new Checkout(new PayPal());
+$checkout->takePayment();
